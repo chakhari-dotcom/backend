5d50334804bb405ac37bffa373e2bc5feeff4f2f
chakhari-dotcom <chakharifatma77@gmail.com>
2024-10-16 22:05:16 +0100

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -7,7 +7,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/todo')
     }
    )
    .catch(
-    ()=>{
-        console.log(err)
+    (error)=>{
+        console.error("error connecting",error)
     }
    )
